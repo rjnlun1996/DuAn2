fce4359959f4f2c8a4acc3aa26bc0a273ff10a05
Trần Thị Tường Vi <vitttps10552@fpt.edu.vn>
2020-12-21 21:37:52 +0700

    Login_chua_hoan_thien

--- ShopDienThoai/src/main/java/com/hitech/controller/ALoginController.java
+++ ShopDienThoai/src/main/java/com/hitech/controller/ALoginController.java
@@ -3,21 +3,32 @@ package com.hitech.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.hitech.constraints.ViewConstraint;
 import com.hitech.services.AccountService;
 
 @Controller
-@RequestMapping("/ho-admin/login")
 public class ALoginController {	
 	
 	@Autowired
 	private AccountService accountService;
 
-	@RequestMapping("")
+	@GetMapping(ViewConstraint.URL_ADMIN_LOGIN)
 	public String table(Model model) {
-		boolean isValidLogin = accountService.loginAdmin("aquoc@fpt.edu.vn", "aquoc");
-		System.out.println(isValidLogin);
-		return "admin/login";
+		return ViewConstraint.VIEW_ADMIN_LOGIN;
+	}
+	
+	@PostMapping(value = ViewConstraint.URL_ADMIN_LOGIN)
+	public String insert(Model model, @RequestParam String username, @RequestParam String password) {
+		boolean isValidLogin = accountService.loginAdmin(username, password);
+		if(!isValidLogin) {
+			model.addAttribute("isValidLogin", isValidLogin);
+			return ViewConstraint.VIEW_ADMIN_LOGIN;
+		}		
+		return "redirect:/ho-admin/";
+		
 	}
 }
