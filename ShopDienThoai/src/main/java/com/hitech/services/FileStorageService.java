5ff59be973171575aff3778c4b164ddf2c165036
quoxank <leanhquoc1710@gmail.com>
2021-01-09 00:11:31 +0700

    upload image by admin

---
+++ ShopDienThoai/src/main/java/com/hitech/services/FileStorageService.java
@@ -0,0 +1,10 @@
+package com.hitech.services;
+
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+	
+	String saveImage(MultipartFile file) throws IOException;
+}
