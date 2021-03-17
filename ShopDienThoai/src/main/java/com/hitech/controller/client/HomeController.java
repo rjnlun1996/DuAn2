b3d3091c03f9011590e83477087addc2ec495ab2
Trần Thị Tường Vi <vitttps10552@fpt.edu.vn>
2021-03-17 21:15:44 +0700

    Login Client

--- ShopDienThoai/src/main/java/com/hitech/controller/client/HomeController.java
+++ ShopDienThoai/src/main/java/com/hitech/controller/client/HomeController.java
@@ -1,13 +1,17 @@
 package com.hitech.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hitech.constraints.CViewConstraint;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value = {"", "index"})
-	public String home() {
-		return "client/index";
+	@RequestMapping(CViewConstraint.URL_HOME)
+	public String table(Model model) {
+		model.addAttribute(CViewConstraint.MENU, CViewConstraint.URL_HOME);
+		return CViewConstraint.VIEW_HOME;
 	}
 }
