f74ed7a85061a658571e695b2371031c4fa50b9d
Trần Thị Tường Vi <vitttps10552@fpt.edu.vn>
2021-03-31 20:18:53 +0700

    gio hang

--- ShopDienThoai/src/main/webapp/js/cart.js
+++ ShopDienThoai/src/main/webapp/js/cart.js
@@ -1,18 +1,48 @@
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
 function themVaoGioHang(productId) {
     $.ajax({
-        url: 'cart',
+        url: 'check-login',
         type: 'POST',
-        data: { productId: productId },
         success: function(data) {
-            if (typeof data === 'string') {
+            if (data === 0) {
                 window.location.href = '/login';
+            } else {
+                $.ajax({
+                    url: 'cart',
+                    type: 'POST',
+                    data: { productId: productId },
+                    success: function(data) {
+                        // var cartNumber = Object.keys(data).length;                         
+                        $('#render-cart').html(data);
+                        $('#cart-number').html($('.dropcart__product').length);
+                        toastr["success"]("Thêm vào giỏ hàng thành công!")
+                    },
+                    error: function(err) {
+
+                    }
+                });
             }
-            var cartNumber = Object.keys(data).length;
-            $('#cart-number').html(cartNumber);
-            console.log(data)
         },
         error: function(err) {
 
         }
     })
+
 }
